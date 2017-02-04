9ea9f24d43b1c1660f09112f685335528a396c9e
Thomas Schaaf <schaaf@komola.de>
2017-02-04 22:53:51 +0100

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -1,3 +1,53 @@
 #!/bin/bash
 cd react
-npm install
+
+function install {
+  # clean cache
+  if [ "$1" = "cold" ]; then
+    yarn cache clean
+  fi
+
+  # remove node_modules
+  if [ "$2" = "empty" ]; then
+    rm -rf node_modules
+  fi
+  
+  # remove node_modules
+  if [ "$3" = "no" ]; then
+    if [ -e "npm-shrinkwrap.json" ]; then
+      mv npm-shrinkwrap.json .npm-shrinkwrap.json
+    fi
+  else
+    if [ -e ".npm-shrinkwrap.json" ]; then
+      mv .npm-shrinkwrap.json npm-shrinkwrap.json
+    fi
+  fi
+  
+  SECONDS=0
+  npm install
+  echo "$SECONDS s CACHE: $1, NODE_MODULES: $2, LOCKFILE: $3
+}
+
+# CACHE: cold, NODE_MODULES: empty, LOCKFILE: no
+install cold empty no
+
+# CACHE: cold, NODE_MODULES: empty, LOCKFILE: yes
+install cold empty yes
+
+# CACHE: cold, NODE_MODULES: installed, LOCKFILE: no
+install cold installed no
+
+# CACHE: cold, NODE_MODULES: installed, LOCKFILE: yes
+install cold installed yes
+
+# CACHE: warm, NODE_MODULES: empty, LOCKFILE: no
+install warm empty no
+
+# CACHE: warm, NODE_MODULES: empty, LOCKFILE: yes
+install warm empty yes
+
+# CACHE: warm, NODE_MODULES: installed, LOCKFILE: no
+install warm installed no
+
+# CACHE: warm, NODE_MODULES: installed, LOCKFILE: yes
+install warm installed yes
